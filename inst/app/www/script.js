cc3cec7c88ccf00f0e8bff000e49684c385269ac
antoine <antoine@thinkr.fr>
2023-11-13 11:34:33 +0000

    feat: adjust map zoom to screen
    - On large screens the map was to tiny with default zoom
    
    - Send screen width from JS to R
    - Adapt zoom based on screen width at app startup

---
+++ inst/app/www/script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+
+ $(document).on('shiny:connected', function(event) {
+    var screen_width = document.documentElement.clientWidth;
+    Shiny.setInputValue("screen_width", screen_width)
+  });
+
+});
