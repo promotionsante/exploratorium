75b8a17a889362bdb3ab84f9f4dce27a5dff7b03
margot <margot@thinkr.fr>
2023-11-14 13:54:40 +0000

    feat: Explore the selection of one project
    Why:
    - We have to capture the selection of a project
    
    What:
    - Observe the click on the point

--- R/mod_map.R
+++ R/mod_map.R
@@ -30,19 +30,31 @@ mod_map_server <- function(id, r_global){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
+    data("toy_projects_data_sf")
+    output$map <- renderLeaflet({
+      draw_map_selected_projects(
+        projects_data_sf = toy_projects_data_sf,
+        zoom_level = r_global$zoom_level
+      )
+    })
+
+    # data("toy_projects_data_sf")
     # output$map <- renderLeaflet({
-    #   draw_map_selected_projects(
-    #     projects_data_sf = dummy_project_data_sf(),
-    #     zoom_level = r_global$zoom_level
+    #   draw_map_focus_one_project(
+    #     projects_data_sf = toy_projects_data_sf,
+    #     project_short_title = "1+1=3  PGV03.038"
     #   )
     # })
 
-    data("toy_projects_data_sf")
-    output$map <- renderLeaflet({
-      draw_map_focus_one_project(
-        projects_data_sf = toy_projects_data_sf,
-        project_short_title = "1+1=3  PGV03.038"
+    observeEvent(
+      input$map_marker_click$id,
+      ignoreNULL = TRUE,
+      ignoreInit = TRUE, {
+
+      print(
+        paste("Project selected:", input$map_marker_click$id)
       )
+
     })
 
   })
